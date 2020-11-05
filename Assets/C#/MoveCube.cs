6b81d07fa46b2df921bbdb566e6e2afa2a5c6b8c
Max <bazuka5801@mail.ru>
2020-11-05 23:15:00 +0300

    feat(MoveCube): add input manager with moveVector

---
+++ Assets/C#/MoveCube.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class MoveCube : MonoBehaviour
+{
+    [SerializeField][Tooltip("WASD move keys")]
+    private KeyCode[] moveKeys = new KeyCode[4];
+    
+    private Vector3[] m_MoveVectors = 
+    {
+        // W - Up
+        new Vector3(0,0,1), 
+        // A - Left
+        new Vector3(-1, 0,0),
+        // S - Down
+        new Vector3(0,0,-1),
+        // D - Right
+        new Vector3(1,0,0), 
+    };
+
+    private Vector3 m_MoveVector = Vector3.zero;
+    private void Update()
+    {
+        m_MoveVector = Vector3.zero;
+        for (var i = 0; i < moveKeys.Length; i++)
+        {
+            if (Input.GetKey(moveKeys[i]))
+                m_MoveVector += m_MoveVectors[i];
+        }
+    }
+}
